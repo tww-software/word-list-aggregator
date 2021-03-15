a5cd7b22d51ddd6b0fd37914573d97c48ad2d6a3
Thomas W Whittam <tw.whittam@gmail.com>
2021-03-15 10:48:58 +0000

    updated docstrings

--- wordlistagg.py
+++ wordlistagg.py
@@ -11,6 +11,18 @@ import os
 
 
 def open_wordlist(path):
+    """
+    open a wordlist
+
+    Note:
+        a wordlist should be a text document with one word per line
+
+    Args:
+        path(str): full path to the wordlist
+
+    Returns:
+        passwordlist(list): list of all the words (as strings) in the wordlist
+    """
     with open(path, 'r', errors='ignore') as wordf:
         passwordlist = []
         for line in wordf:
@@ -19,12 +31,26 @@ def open_wordlist(path):
 
 
 def create_wordlist(wordlist, outpath):
+    """
+    create our own wordlist
+    write each word on a new line in a text file
+
+    Args:
+        wordlist(list): list of strings (words) to write to file
+        outpath(str): full path to write wordlist to
+    """
     with open(outpath, 'w') as wordout:
         for line in wordlist:
             wordout.write(line)
 
 
 def parse_cli_args():
+    """
+    parse arguments off the command line
+
+    Returns:
+        args(argparse.Namespace): the CLI arguments
+    """
     parser = argparse.ArgumentParser(
         'combine all the unique passwords of several password lists into one')
     parser.add_argument('inputdir', help='directory containing wordlists')
@@ -34,6 +60,9 @@ def parse_cli_args():
 
 
 def main():
+    """
+    main program code
+    """
     totalpasswords = 0
     bigset = set()
     args = parse_cli_args()
